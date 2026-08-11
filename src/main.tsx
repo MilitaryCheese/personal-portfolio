7a187c394e37e9e1b5fb26e878d2785671a99a89
Keshani Jayasinghe <keshinijaya@gmail.com>
2026-08-11 11:58:04 +1000

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './styles/globals.css'
 import './styles/crt.css'
 import './styles/typography.css'
@@ -8,5 +9,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
